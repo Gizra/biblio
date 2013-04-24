b1c667730d4fea7b1194f7a9f8c356a81ba8293d
Amitaibu <amitai@gizra.com>
2013-04-24 15:24:32 +0300

    Add citeproc.

---
+++ plugins/biblio_style/citeproc/BiblioStyleCiteProc.class.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * @file
+ * Chicago biblio style.
+ */
+
+class BiblioStyleCiteProc extends BiblioStyleBase {
+
+  public function render($options = array()) {
+    return 'CiteProc';
+  }
+
+}
