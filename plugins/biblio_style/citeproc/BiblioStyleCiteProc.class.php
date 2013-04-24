b3180e8e366ef0372107cdf908d428c6b66e41e0
Amitaibu <amitai@gizra.com>
2013-04-24 16:10:12 +0300

    fix cireproc.

--- plugins/biblio_style/citeproc/BiblioStyleCiteProc.class.php
+++ plugins/biblio_style/citeproc/BiblioStyleCiteProc.class.php
@@ -11,13 +11,19 @@ class BiblioStyleCiteProc extends BiblioStyleBase {
     global $language;
     $langcode = $langcode ? $langcode : $language->language;
 
+    dpm($this->plugin);
+
     // Make sure the CSL file exists.
-    $style_name = $options['style_name'];
+    $style_name = $this->plugin['options']['style_name'] . '.csl';
     // @todo: Allow adding more styles in the Library.
-    $file_path = !empty($options['style_path']) ? $options['style_path'] : './styles/' . $style_name . '.csl';
+    $file_path = $this->plugin['options']['style_path'] . '/' . $style_name;
     if (!file_exists($file_path)) {
-      throw new Exception(format_string('@style file does not exist.', array('@style' => $style_name)));
+      throw new Exception(format_string('@style file does not exist in @path.', array('@style' => $style_name, '@path' => $file_path)));
     }
+
+    $csl_file_contents = file_get_contents($file_path);
+
+    return 'Citeproc';
 
     $citeproc = new citeproc($csl_file_contents, $langcode);
 
