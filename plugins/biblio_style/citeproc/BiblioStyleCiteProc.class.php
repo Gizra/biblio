b59195b2991ff0297287c01b0a64aaeda73c5dd2
Amitaibu <amitai@gizra.com>
2013-04-29 14:42:26 +0300

    Fix cardinality.

--- plugins/biblio_style/citeproc/BiblioStyleCiteProc.class.php
+++ plugins/biblio_style/citeproc/BiblioStyleCiteProc.class.php
@@ -26,90 +26,128 @@ class BiblioStyleCiteProc extends BiblioStyleBase {
     $citeproc = new citeproc($csl_file_contents, $langcode);
 
     // Pass CiteProc the mapped biblio.
-    return $citeproc->render($this->map());
+    $mapped_data = $this->map();
+    return $citeproc->render($mapped_data);
   }
 
 
+  /**
+   * Map the fields from the Biblio entity to the ones known by CiteProc.
+   */
   public function map() {
     $this->mappedBiblio = new stdClass();
     $mapping = $this->getMapping();
     $wrapper = entity_metadata_wrapper('biblio', $this->biblio);
-    foreach ($mapping['field'] as $key => $value) {
+    foreach ($mapping['biblio']['field'] as $key => $value) {
       if (!isset($wrapper->{$value})) {
         continue;
       }
 
       $this->mappedBiblio->{$key} = $wrapper->{$value}->value();
     }
+
+    // Add contribuots.
+    if (isset($wrapper->contributor_collection) && $contributors = $wrapper->contributor_collection->value()) {
+      foreach ($contributors as $contributor) {
+        continue;
+        $type = $contributor->type;
+
+        $mapped_contributor = new stdClass();
+        $contributor_wrapper = entity_metadata_wrapper('biblio_contributor', $contributor);
+
+        // Map the contributor data.
+        foreach ($mapping['contributor']['field'] as $contributor_key => $contributor_value) {
+          if (!isset($wrapper->{$value})) {
+            continue;
+          }
+
+          $mapped_contributor = $contributor_wrapper->{$value}->value();
+        }
+
+        if ($mapped_contributor) {
+          $this->mappedBiblio->{$type}[] = $mapped_contributor;
+        }
+      }
+    }
+
     return $this->mappedBiblio;
   }
 
   public function getMapping() {
     return array(
-      'field' => array(
-        'title' => 'title',
-        'container-title' => 'container-title',
-        'collection-title' => 'collection-title',
-        'original-title' => 'original-title',
-        'publisher' => 'publisher',
-        'publisher-place' => 'publisher-place',
-        'original-publisher' => 'original-publisher',
-        'original-publisher-place' => 'original-publisher-place',
-        'archive' => 'archive',
-        'archive-place' => 'archive-place',
-        'authority' => 'authority',
-        'archive_location' => 'authority',
-        'event' => 'event',
-        'event-place' => 'event-place',
-        'page' => 'biblio_pages',
-        'page-first' => 'page',
-        'locator' => 'locator',
-        'version' => 'version',
-        'volume' => 'volume',
-        'number-of-volumes' => 'number-of-volumes',
-        'number-of-pages' => 'number-of-pages',
-        'issue' => 'issue',
-        'chapter-number' => 'chapter-number',
-        'medium' => 'medium',
-        'status' => 'status',
-        'edition' => 'edition',
-        'section' => 'section',
-        'genre' => 'genre',
-        'note' => 'note',
-        'annote' => 'annote',
-        'abstract'  => 'abstract',
-        'keyword' => 'keyword',
-        'number' => 'number',
-        'references' => 'references',
-        'URL' => 'URL',
-        'DOI' => 'DOI',
-        'ISBN' => 'ISBN',
-        'call-number' => 'call-number',
-        'citation-number' => 'citation-number',
-        'citation-label' => 'citation-label',
-        'first-reference-note-number' => 'first-reference-note-number',
-        'year-suffix' => 'year-suffix',
-        'jurisdiction' => 'jurisdiction',
+      'biblio' => array(
+        'field' => array(
+          'title' => 'title',
+          'container-title' => 'container-title',
+          'collection-title' => 'collection-title',
+          'original-title' => 'original-title',
+          'publisher' => 'publisher',
+          'publisher-place' => 'publisher-place',
+          'original-publisher' => 'original-publisher',
+          'original-publisher-place' => 'original-publisher-place',
+          'archive' => 'archive',
+          'archive-place' => 'archive-place',
+          'authority' => 'authority',
+          'archive_location' => 'authority',
+          'event' => 'event',
+          'event-place' => 'event-place',
+          'page' => 'biblio_pages',
+          'page-first' => 'page',
+          'locator' => 'locator',
+          'version' => 'version',
+          'volume' => 'volume',
+          'number-of-volumes' => 'number-of-volumes',
+          'number-of-pages' => 'number-of-pages',
+          'issue' => 'issue',
+          'chapter-number' => 'chapter-number',
+          'medium' => 'medium',
+          'status' => 'status',
+          'edition' => 'edition',
+          'section' => 'section',
+          'genre' => 'genre',
+          'note' => 'note',
+          'annote' => 'annote',
+          'abstract'  => 'abstract',
+          'keyword' => 'keyword',
+          'number' => 'number',
+          'references' => 'references',
+          'URL' => 'URL',
+          'DOI' => 'DOI',
+          'ISBN' => 'ISBN',
+          'call-number' => 'call-number',
+          'citation-number' => 'citation-number',
+          'citation-label' => 'citation-label',
+          'first-reference-note-number' => 'first-reference-note-number',
+          'year-suffix' => 'year-suffix',
+          'jurisdiction' => 'jurisdiction',
 
-        //Date Variables'
-        'issued' => 'issued',
-        'event' => 'event',
-        'accessed' => 'accessed',
-        'container' => 'container',
-        'original-date' => 'original-date',
+          //Date Variables'
+          'issued' => 'issued',
+          'event' => 'event',
+          'accessed' => 'accessed',
+          'container' => 'container',
+          'original-date' => 'original-date',
 
-        //Name Variables'
-        'author' => 'author',
-        'editor' => 'editor',
-        'translator' => 'translator',
-        'recipient' => 'recipient',
-        'interviewer' => 'interviewer',
-        'publisher' => 'publisher',
-        'composer' => 'composer',
-        'original-publisher' => 'original-publisher',
-        'original-author' => 'original-author',
-        'container-author' => 'container-author',
-        'collection-editor' => 'collection-editor',
+          //Name Variables'
+          'author' => 'author',
+          'editor' => 'editor',
+          'translator' => 'translator',
+          'recipient' => 'recipient',
+          'interviewer' => 'interviewer',
+          'publisher' => 'publisher',
+          'composer' => 'composer',
+          'original-publisher' => 'original-publisher',
+          'original-author' => 'original-author',
+          'container-author' => 'container-author',
+          'collection-editor' => 'collection-editor',
+        ),
+      ),
+      'contributor' => array(
+        'field' => array(
+          'initials' => 'contributor_initials',
+          'given' => 'contributor_given',
+          'family' => 'contributor_family',
+        ),
       ),
       'type' => array(
         'article' => 'article',
