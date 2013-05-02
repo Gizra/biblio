d1e974f6bb4171a672d347d2cac604ce4ce5c9f8
Amitaibu <amitai@gizra.com>
2013-05-02 16:48:11 +0300

    Remove files.

--- misc/biblio.highlight.js
+++
@@ -1,9 +0,0 @@
-(function ($) {
-  Drupal.behaviors.BiblioHighlight = {
-    attach: function (context, settings) {
-      $('input#biblio-highlight', context).click(function(e) {
-        $("div.suspect").toggleClass('biblio-highlight');
-      });
-    }
-  };
-}(jQuery));
