68db9b79065061f54eff985403aec69edb40e599
Amitaibu <amitai@gizra.com>
2013-06-18 12:52:12 +0300

    Add field mapping.

--- plugins/biblio_style/bibtex/BiblioStyleBibtex.class.php
+++ plugins/biblio_style/bibtex/BiblioStyleBibtex.class.php
@@ -50,25 +50,25 @@ class BiblioStyleBibtex extends BiblioStyleBase {
 
     $bibtex .= '@' . $type . ' {';
     $bibtex .= ($biblio->biblio_citekey) ? $biblio->biblio_citekey  : "";
-    $bibtex .= $this->formatEntry('title', $biblio->title);
+    $bibtex .= $this->formatEntry('title');
     $bibtex .= $this->formatEntry('journal', $journal);
     $bibtex .= $this->formatEntry('booktitle', $booktitle);
     $bibtex .= $this->formatEntry('series', $series);
-    $bibtex .= $this->formatEntry('volume', $biblio->biblio_volume);
-    $bibtex .= $this->formatEntry('number', $biblio->biblio_number);
-    $bibtex .= $this->formatEntry('year', $biblio->biblio_year);
-    $bibtex .= $this->formatEntry('note', $biblio->biblio_notes);
-    $bibtex .= $this->formatEntry('month', $biblio->biblio_date);
-    $bibtex .= $this->formatEntry('pages', $biblio->biblio_pages);
-    $bibtex .= $this->formatEntry('publisher', $biblio->biblio_publisher);
+    $bibtex .= $this->formatEntry('volume');
+    $bibtex .= $this->formatEntry('number');
+    $bibtex .= $this->formatEntry('year');
+    $bibtex .= $this->formatEntry('note');
+    $bibtex .= $this->formatEntry('month');
+    $bibtex .= $this->formatEntry('pages');
+    $bibtex .= $this->formatEntry('publisher');
     $bibtex .= $this->formatEntry('school', $school);
     $bibtex .= $this->formatEntry('organization', $organization);
     $bibtex .= $this->formatEntry('institution', $institution);
-    $bibtex .= $this->formatEntry('type', $biblio->biblio_type_of_work);
-    $bibtex .= $this->formatEntry('edition', $biblio->biblio_edition);
-    $bibtex .= $this->formatEntry('chapter', $biblio->biblio_section);
-    $bibtex .= $this->formatEntry('address', $biblio->biblio_place_published);
-    $bibtex .= $this->formatEntry('abstract', $biblio->biblio_abst_e);
+    $bibtex .= $this->formatEntry('type');
+    $bibtex .= $this->formatEntry('edition');
+    $bibtex .= $this->formatEntry('chapter');
+    $bibtex .= $this->formatEntry('address');
+    $bibtex .= $this->formatEntry('abstract');
 
     $kw_array = array();
     if (!empty($biblio->terms)) {
@@ -86,10 +86,10 @@ class BiblioStyleBibtex extends BiblioStyleBase {
       $bibtex .= $this->formatEntry('keywords', implode(', ', $kw_array));
     }
 
-    $bibtex .= $this->formatEntry('isbn', $biblio->biblio_isbn);
-    $bibtex .= $this->formatEntry('issn', $biblio->biblio_issn);
-    $bibtex .= $this->formatEntry('doi', $biblio->biblio_doi);
-    $bibtex .= $this->formatEntry('url', $biblio->biblio_url);
+    $bibtex .= $this->formatEntry('isbn');
+    $bibtex .= $this->formatEntry('issn');
+    $bibtex .= $this->formatEntry('doi');
+    $bibtex .= $this->formatEntry('url');
 
     if (!empty ($biblio->upload) && count($biblio->upload['und']) && user_access('view uploaded files')) {
       foreach ($biblio->upload['und'] as $file) {
@@ -107,8 +107,13 @@ class BiblioStyleBibtex extends BiblioStyleBase {
     }
     $a = implode(' and ', $a);
     $e = implode(' and ', $e);
-    if (!empty ($a)) $bibtex .= $this->formatEntry('author', $a);
-    if (!empty ($e)) $bibtex .= $this->formatEntry('editor', $e);
+    if (!empty ($a)) {
+      $bibtex .= $this->formatEntry('author', $a);
+    }
+    if (!empty ($e)) {
+      $bibtex .= $this->formatEntry('editor', $e);
+    }
+
     $bibtex .= "\n}\n";
 
 
@@ -122,8 +127,39 @@ class BiblioStyleBibtex extends BiblioStyleBase {
     return $bibtex;
   }
 
-  private function formatEntry($key, $value) {
-    return !empty($value) ? ",\n\t$key = {" . $value . "}" : '';
+  /**
+   * Format an entry.
+   *
+   * @param $key
+   *   The BibTeX key name.
+   * @param $value
+   *   Optional; The value to format. If empty, try to get it from the Biblio
+   *   entity. Defaults to NULL.
+   * @return string
+   */
+  private function formatEntry($key, $value = NULL) {
+    if (empty($value)) {
+      $map = $this->getMapping();
+      $map = $map['field'];
+
+      if (empty($map[$key])) {
+        return;
+      }
+
+      // @todo: Cache the wrapper.
+      $wrapper = entity_metadata_wrapper('biblio', $this->biblio);
+
+      $property_name = $map[$key];
+      if (!isset($wrapper->{$property_name})) {
+        return;
+      }
+
+      if (!$value = $wrapper->{$property_name}->value()) {
+        return;
+      }
+    }
+
+    return ",\n\t$key = {" . $value . "}";
   }
 
   /**
@@ -146,84 +182,29 @@ class BiblioStyleBibtex extends BiblioStyleBase {
 
   public function getMapping() {
     return array(
-      'biblio' => array(
-        'text' => array(
-          // Text variables.
-          'title' => 'title',
-          'container-title' => 'container-title',
-          'collection-title' => 'collection-title',
-          'original-title' => 'original-title',
-          'publisher' => 'publisher',
-          'publisher-place' => 'publisher-place',
-          'original-publisher' => 'original-publisher',
-          'original-publisher-place' => 'original-publisher-place',
-          'archive' => 'archive',
-          'archive-place' => 'archive-place',
-          'authority' => 'authority',
-          'archive_location' => 'authority',
-          'event' => 'event',
-          'event-place' => 'event-place',
-          'page' => 'biblio_pages',
-          'page-first' => 'page',
-          'locator' => 'locator',
-          'version' => 'version',
-          'volume' => 'volume',
-          'number-of-volumes' => 'number-of-volumes',
-          'number-of-pages' => 'number-of-pages',
-          'issue' => 'issue',
-          'chapter-number' => 'chapter-number',
-          'medium' => 'medium',
-          'status' => 'status',
-          'edition' => 'edition',
-          'section' => 'section',
-          'genre' => 'genre',
-          'note' => 'note',
-          'annote' => 'annote',
-          'abstract'  => 'abstract',
-          'keyword' => 'keyword',
-          'number' => 'number',
-          'references' => 'references',
-          'URL' => 'URL',
-          'DOI' => 'DOI',
-          'ISBN' => 'ISBN',
-          'call-number' => 'call-number',
-          'citation-number' => 'citation-number',
-          'citation-label' => 'citation-label',
-          'first-reference-note-number' => 'first-reference-note-number',
-          'year-suffix' => 'year-suffix',
-          'jurisdiction' => 'jurisdiction',
-        ),
-
-        'date' => array(
-          // Date Variables.
-          'issued' => 'biblio_issued',
-          'event' => 'event',
-          'accessed' => 'accessed',
-          'container' => 'container',
-          'original-date' => 'original-date',
-        ),
-
-        'contributor' => array(
-          // Contributor Variables.
-          'author' => 'author',
-          'editor' => 'editor',
-          'translator' => 'translator',
-          'recipient' => 'recipient',
-          'interviewer' => 'interviewer',
-          'publisher' => 'publisher',
-          'composer' => 'composer',
-          'original-publisher' => 'original-publisher',
-          'original-author' => 'original-author',
-          'container-author' => 'container-author',
-          'collection-editor' => 'collection-editor',
-        ),
+      'type' => array(
       ),
-      'contributor' => array(
-        'text' => array(
-          'initials' => 'contributor_initials',
-          'given' => 'contributor_given',
-          'family' => 'contributor_family',
-        ),
+      // Array with field mapping, keyed by BibTeX name, and the Biblio field
+      // as the value.
+      'field' => array(
+        'title' => 'title',
+        'volume' => 'biblio_volume',
+        'number' => 'biblio_number',
+        'year' => 'biblio_year',
+        'note' => 'biblio_notes',
+        'month' => 'biblio_date',
+        'pages' => 'biblio_pages',
+        'publisher' => 'biblio_publisher',
+        'type' => 'biblio_type_of_work',
+        'edition' => 'biblio_edition',
+        'chapter' => 'biblio_section',
+        'address' => 'biblio_place_published',
+        'abstract' => 'biblio_abstract',
+        'isbn' => 'biblio_isbn',
+        'issn' => 'biblio_issn',
+        'doi' => 'biblio_doi',
+        // @todo: Is this the Biblio URL?
+        'url' => 'biblio_url',
       ),
     );
   }
