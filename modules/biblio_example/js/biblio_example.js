03683d81f3f79226c1c25e6843fc9aa3aa7686e0
Ran Rosenberg <ran@gizra.com>
2013-07-31 12:29:15 +0300

    Fix comments.

--- modules/biblio_example/js/biblio_example.js
+++ modules/biblio_example/js/biblio_example.js
@@ -1,18 +1,15 @@
 (function($) {
 
-// Controls the display of the "Abstract" field of a publication.
+/**
+* Controls the display of the "Abstract" field of a publication.
+*/
 Drupal.behaviors.toggleAbstract = {
   attach: function() {
     $(".show-abstract").click(function() {
 
       var bid = $(this).attr('bid');
 
-      if ($(this).hasClass('open')){
-        $(this).removeClass('open');
-      }
-      else {
-        $(this).addClass('open');
-      }
+      $(this).toggleClass('open');
 
       $("div .bid-" + bid).slideToggle("fast");
     });
