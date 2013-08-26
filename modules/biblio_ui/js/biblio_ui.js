929bb3683ec72a256cd5d81d8d2243662730c2c1
RoySegall <roy.segall@gizra.com>
2013-08-26 15:52:38 +0300

    Mreging Biblio contributior UI into biblio UI.

--- modules/biblio_ui/js/biblio_ui.js
+++ modules/biblio_ui/js/biblio_ui.js
@@ -1,10 +1,23 @@
-
 (function ($) {
 
   Drupal.behaviors.biblioUiFieldsetSummaries = {
     attach: function (context) {
       $('fieldset.biblio-form-owner', context).drupalSetSummary(function (context) {
         var name = $('.form-item-name input', context).val();
+
+        return Drupal.t('By @name', { '@name': name });
+      });
+    }
+  };
+
+})(jQuery);
+
+(function ($) {
+
+  Drupal.behaviors.biblioContributorUiFieldsetSummaries = {
+    attach: function (context) {
+      $('fieldset.biblio-form-owner', context).drupalSetSummary(function (context) {
+        var name = $('.form-item-publisher input', context).val();
 
         return Drupal.t('By @name', { '@name': name });
       });
