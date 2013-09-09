1bd75d9eff8a513e627d14b3d10a075e0388f6f0
Amitaibu <amitai@gizra.com>
2013-09-09 11:14:21 +0300

    Add mapping.

--- plugins/biblio_style/endnote/BiblioStyleEndNoteXML7.php
+++ plugins/biblio_style/endnote/BiblioStyleEndNoteXML7.php
@@ -18,130 +18,10 @@ class BiblioStyleEndNoteXML7 extends BiblioStyleEndNoteXML8 {
     return parent::import($data, $options);
   }
 
-  function endNote7StartElement($parser, $name, $attrs) {
-    switch ($name) {
-      case 'RECORD' :
-        $this->biblio = new stdClass();
-        $this->biblio->biblio_contributors = array();
-        $this->biblio->biblio_type = 102; // we set 102 here because the xml parser won't
-        // process a value of 0 (ZERO) which is the
-        // ref-type 102. if there is a non-zero value it will be overwritten
-        $this->element = '';
-        break;
-      case 'AUTHORS':
-      case 'SECONDARY_AUTHORS':
-      case 'TERTIARY_AUTHORS':
-      case 'SUBSIDIARY_AUTHORS':
-        $this->contrib_count = 0;
-        $this->contributors = array();
-        break;
-      case 'AUTHOR':
-      case 'SECONDARY_AUTHOR':
-      case 'TERTIARY_AUTHOR':
-      case 'SUBSIDIARY_AUTHOR':
-        $this->contributors[$this->contrib_count]['name'] = '';
-        $this->element = $name;
-        break;
-      case 'KEYWORDS':
-        $this->keyword_count = 0;
-        break;
-      case 'KEYWORD':
-        $this->biblio->biblio_keywords[$this->keyword_count] = '';
-        $this->element = $name;
-        break;
-      default:
-        $this->element = $name;
-    }
-  }
-
-  function endNote7EndElement($parser, $name) {
-    switch ($name) {
-      case 'RECORD' :
-        $this->biblio->biblio_xml_md5 = md5(serialize($this->biblio));
-        if ( !($dup = $this->biblio_xml_check_md5($this->biblio->biblio_xml_md5)) ) {
-          biblio_save_node($this->biblio, $this->terms, $this->batch_proc, $this->session_id);
-          if (!empty($this->biblio->nid)) $this->nids[] = $this->biblio->nid;
-        }
-        else {
-          $this->dups[] = $dup;
-        }
-        break;
-      case 'AUTHORS':
-      case 'SECONDARY_AUTHORS':
-      case 'TERTIARY_AUTHORS':
-      case 'SUBSIDIARY_AUTHORS':
-        $this->contributors_type = '';
-        foreach ($this->contributors as $contributor) {
-          $this->biblio->biblio_contributors[] = $contributor;
-        }
-        break;
-      case 'AUTHOR':
-        $this->contributors[$this->contrib_count]['auth_category'] = 1;
-        $this->contributors[$this->contrib_count]['auth_type'] = 1;
-        $this->contrib_count++;
-        break;
-      case 'SECONDARY_AUTHOR':
-        $this->contributors[$this->contrib_count]['auth_category'] = 2;
-        $this->contributors[$this->contrib_count]['auth_type'] = 2;
-        $this->contrib_count++;
-        break;
-      case 'TERTIARY_AUTHOR':
-        $this->contributors[$this->contrib_count]['auth_category'] = 3;
-        $this->contributors[$this->contrib_count]['auth_type'] = 3;
-        $this->contrib_count++;
-        break;
-      case 'SUBSIDIARY_AUTHOR':
-        $this->contributors[$this->contrib_count]['auth_category'] = 4;
-        $this->contributors[$this->contrib_count]['auth_type'] = 4;
-        $this->contrib_count++;
-        break;
-      case 'KEYWORD':
-        $this->keyword_count++;
-        break;
-      default:
-
-    }
-    $this->element = '';
-  }
-
-  function endNote7CharacterData($parser, $data) {
-    if (trim($data)) {
-      switch ($this->element) {
-        case 'REFERENCE_TYPE':
-          $this->biblio->biblio_type = $this->type_map($data);
-          break;
-        case 'AUTHOR':
-        case 'SECONDARY_AUTHOR':
-        case 'TERTIARY_AUTHOR':
-        case 'SUBSIDIARY_AUTHOR':
-          $this->contributors[$this->contrib_count]['name'] .= $data;
-          break;
-        case 'KEYWORD':
-          $this->biblio->biblio_keywords[$this->keyword_count] .= $data;
-          break;
-        case 'TITLE':
-          $this->biblio->title .= $data;
-          break;
-        default:
-          if ($field = $this->field_map(trim($this->element))) {
-            $this->biblio->$field .= $data;
-          }
-          else {
-            if (!in_array($this->element, $this->unmapped)) {
-              $this->unmapped[] = $this->element;
-            }
-          }
-      }
-    }
-  }
-
-  public function field_map() {}
-
-  public function type_map() {}
-
-
   /**
    * Render tagged.
+   *
+   * @todo: Remove option to render with XML7.
    */
   public function render($options = array(), $langcode = NULL) {
 
@@ -203,6 +83,40 @@ class BiblioStyleEndNoteXML7 extends BiblioStyleEndNoteXML8 {
         'YEAR' => array('property' => 'biblio_year'),
       ),
     );
+
+    $parent_map = parent::getMapping();
+
+    $info = array(
+      'ABSTRACT' => 'abstract',
+      'ACCESSION_NUMBER' => 'accession-num',
+      'ALTERNATE_TITLE' => 'alt-title',
+      'CALL_NUMBER' => 'call-num',
+      'EDITION' => 'edition',
+      'ISBN' => 'isbn',
+      'ISSUE' => 'issue',
+      'LABEL' => 'label',
+      'NOTES' => 'notes',
+      'NUMBER' => 'number',
+      'NUMBER_OF_VOLUMES' => 'num-vols',
+      'ORIGINAL_PUB' => 'orig-pub',
+      'PAGES' => 'pages',
+      'PLACE_PUBLISHED' => 'pub-location',
+      'PUBLISHER' => 'publisher',
+      'REPRINT_EDITION' => 'reprint-edition',
+      'SECONDARY_TITLE' => 'secondary-title',
+      'SECTION' => 'section',
+      'SHORT_TITLE' => 'short-title',
+      'TERTIARY_TITLE' => 'tertiary-title',
+      'TYPE_OF_WORK' => 'work-type',
+      'URL' => 'url',
+      'VOLUME' => 'volume',
+      'YEAR' => 'year',
+    );
+
+    foreach ($info as $xml7 => $xml8) {
+      // Map the field info using the XML7 key but with the XML8 definitions.
+      $return['field'][$xml7] = $parent_map['field'][$xml8];
+    }
 
     return $return;
   }
