842c1d91d38e72c02c5c35201fc7af405c88cafd
Amitaibu <amitai@gizra.com>
2013-09-30 11:37:05 +0300

    #15: Add PubMed import and test.

--- plugins/biblio_style/pubmed/BiblioStylePubmed.class.php
+++ plugins/biblio_style/pubmed/BiblioStylePubmed.class.php
@@ -16,86 +16,303 @@ class BiblioStylePubmed extends BiblioStyleBase {
    * @param string $type
    * @return array
    */
-  public function import($data, $type = 'text') {
-    $xml = $data;
-
-    $pubmed = new BiblioEntrezPubmedArticle();
+  public function importData($data, $type = 'text') {
+    $xml = new SimpleXMLElement($data);
 
     // Array of Biblios.
     $biblios = array();
+    $mapping = $this->getMapping();
 
     foreach ($xml->xpath('//PubmedArticle') as $article) {
 
-      $biblio = biblio_create('article');
+      $biblio = biblio_create('journal');
       $wrapper = entity_metadata_wrapper('biblio', $biblio);
 
-      $wrapper->title->set($article->Article->ArticleTitle);
-
-      // Attempts to extract the name of the journal from MedlineTA if
-      // available.
-      if (!empty($article->MedlineJournalInfo->MedlineTA)) {
-        $secondary_title = $article->MedlineJournalInfo->MedlineTA;
-      }
-      elseif (!empty($article->Article->Journal->ISOAbbreviation)) {
-        $secondary_title = $article->Article->Journal->ISOAbbreviation;
-      }
-      else {
-        $secondary_title = $article->Article->Journal->Title;
-      }
-      $wrapper->biblio_secondary_title->set($secondary_title);
-
-      $citekey = variable_get('biblio_auto_citekey', TRUE) ? '' : $article->MedlineCitation->PMID;
-      $wrapper->biblio_citekey->set($citekey);
-
-      $wrapper->biblio_volume->set($article->Article->Journal->JournalIssue->Volume);
-      $wrapper->biblio_issue->set($article->Article->Journal->JournalIssue->Issue);
-      $wrapper->biblio_issn->set($article->Article->Journal->ISSN);
-      $wrapper->biblio_pages->set($article->Article->Pagination->MedlinePgn);
-
-      $pub_date = $article->Article->Journal->JournalIssue->PubDate;
-
-      // @todo: Return timestamp?
-      $date = isset($pub_date->MedlineDate) ? $pub_date->MedlineDate : implode(' ', (array)$pub_date);
-
-      $wrapper->biblio_issued->set($date);
-
-      // @todo: Use date()?
-      // @todo: move this logic to the controller?
-      $wrapper->biblio_year->set(substr($date, 0, 4));
-
-      if (isset($article->Article->Abstract)) {
-        $abstract = '';
-        foreach ($article->Article->Abstract->AbstractText as $text) {
-          if (!empty($abstract)) {
-            $abstract .= "\n\n";
-          }
-          $attrs = $text->attributes();
-          if (isset($attrs['Label'])) {
-            $abstract .= $attrs['Label'] . ': ';
-          }
-          $abstract .= $text ;
+      foreach ($mapping['field'] as $property_name => $property) {
+        if (empty($wrapper->{$property_name})) {
+          // @todo: Is this the right place.
+          biblio_create_field($property_name, 'biblio', $wrapper->getBundle());
         }
-        $abstract;
+
+        $method = $property['import_method'];
+        $this->{$method}($wrapper, $property_name, $article->MedlineCitation, $article->PubmedData);
       }
 
-      $wrapper->biblio_abstract->set($abstract);
+      $this->importContributors($biblio, $article->MedlineCitation);
 
-      $this->biblio = array(
-        'biblio_pubmed_id' => $this->id,
-        'biblio_pubmed_md5' => $this->md5,
-        'biblio_contributors' => $this->contributors(),
-        // MedlineCitations are always articles from journals or books
-        'biblio_custom1'  => "http://www.ncbi.nlm.nih.gov/pubmed/{$this->id}?dopt=Abstract",
-        'biblio_keywords' => $this->keywords(),
-        'biblio_lang'     => $this->lang(),
-      );
-
-      $wrapper->save();
-      $biblios[] = $biblio;
+      $biblios['success'][] = $biblio;
     }
 
-    return array(
-      'new' => $biblios,
+    return $biblios;
+  }
+
+  /**
+   * Import a generic property.
+   *
+   * @param EntityMetadataWrapper $wrapper
+   *   The wrapped Biblio.
+   * @param $property_name
+   *   The propery name (e.g. biblio_year).
+   * @param $data
+   *   A single PubMed article to be processed.
+   * @param $pubmed_data
+   *   A single PubMed article data extracted from "PubmedData".
+   */
+  public function importEntryGeneric(EntityMetadataWrapper $wrapper, $property_name, SimpleXMLElement $data, SimpleXMLElement $pubmed_data) {
+    $mapping = $this->getMapping();
+    $property = $mapping['field'][$property_name];
+
+    // Drill into the object until we have reached our property.
+    $sub_data = $data;
+
+    foreach ($property['import_location'] as $location) {
+      if (empty($sub_data->{$location})) {
+        return;
+      }
+      $sub_data = $sub_data->{$location};
+    }
+
+    $wrapper->{$property_name}->set((string)$sub_data);
+  }
+
+  /**
+   * Import abstract property.
+   *
+   * @param EntityMetadataWrapper $wrapper
+   *   The wrapped Biblio.
+   * @param $property_name
+   *   The propery name (e.g. biblio_abstract).
+   * @param $data
+   *   A single PubMed article to be processed.
+   * @param $pubmed_data
+   *   A single PubMed article data extracted from "PubmedData".
+   */
+  public function importAbstract(EntityMetadataWrapper $wrapper, $property_name, SimpleXMLElement $data, SimpleXMLElement $pubmed_data) {
+    if (!isset($data->Article->Abstract)) {
+      return;
+    }
+
+    $abstract = array();
+    foreach ($data->Article->Abstract->AbstractText as $text) {
+      $output = '';
+      $attrs = $text->attributes();
+      if (isset($attrs['Label'])) {
+        $output = $attrs['Label'] . ': ';
+      }
+      $output .= (string) $text;
+      $abstract[] =  $output;
+    }
+    $wrapper->{$property_name}->set(implode("\n", $abstract));
+  }
+
+  /**
+   * Import contributors.
+   *
+   * @param Biblio $biblio
+   *   The Biblio object.
+   * @param SimpleXMLElement $data
+   *   A single PubMed article to be processed.
+   */
+  public function importContributors(Biblio $biblio, SimpleXMLElement $data) {
+    if (!isset($data->Article->AuthorList->Author)) {
+      return;
+    }
+    $contributors = array();
+    foreach ($data->Article->AuthorList->Author as $author) {
+      $values = array();
+      if (isset($author->CollectiveName)) {
+        // @todo.
+        $category = 5; // corporate author
+        $values['lastname'] = (string)$author->CollectiveName;
+
+      }
+      else {
+        $category = 1; //primary (human) author
+        $values['lastname'] = (string)$author->LastName;
+        if (isset($author->ForeName)) {
+          $values['firstname'] = (string) $author->ForeName;
+        }
+        elseif (isset($author->FirstName)) {
+          $values['firstname'] = (string) $author->FirstName;
+        }
+
+        if (isset($author->Initials)) {
+          $values['initials'] = (string) $author->Initials;
+        }
+      }
+      $contributor = biblio_contributor_create($values);
+      $contributors[] = $this->getBiblioContributor($contributor);
+    }
+
+    $this->addBiblioContributorsToCollection($biblio, $contributors, 'Author');
+
+  }
+
+  /**
+   * Import DOI property.
+   *
+   * @param EntityMetadataWrapper $wrapper
+   *   The wrapped Biblio.
+   * @param $property_name
+   *   The propery name (e.g. biblio_abstract).
+   * @param $data
+   *   A single PubMed article to be processed.
+   * @param $pubmed_data
+   *   A single PubMed article data extracted from "PubmedData".
+   */
+  public function importDoi(EntityMetadataWrapper $wrapper, $property_name, SimpleXMLElement $data, SimpleXMLElement $pubmed_data) {
+    if (!$doi = $data->xpath('.//ELocationID[@EIdType="doi"]/text()')) {
+      $doi = $pubmed_data->xpath('.//ArticleId[@IdType="doi"]/text()');
+    }
+
+    $wrapper->{$property_name}->set((string)$doi[0]);
+  }
+
+  /**
+   * Import keywords property.
+   *
+   * @param EntityMetadataWrapper $wrapper
+   *   The wrapped Biblio.
+   * @param $property_name
+   *   The propery name (e.g. biblio_keywords).
+   * @param $data
+   *   A single PubMed article to be processed.
+   * @param $pubmed_data
+   *   A single PubMed article data extracted from "PubmedData".
+   */
+  public function importKeywords(EntityMetadataWrapper $wrapper, $property_name, SimpleXMLElement $data, SimpleXMLElement $pubmed_data) {
+    if (!isset($data->MeshHeadingList->MeshHeading)) {
+      return;
+    }
+    $keywords = array();
+    foreach ($data->MeshHeadingList->MeshHeading as $heading) {
+      $keywords[] = (string)$heading->DescriptorName;
+    }
+    parent::importKeywords($wrapper, $property_name, $keywords);
+  }
+
+
+  /**
+   * Import year property.
+   *
+   * @todo: Is this assumption ok?
+   * According to PubMed that date might be a string or an XML. We take the
+   * first 4 digits as the year,as Biblio year field is an integer field.
+   *
+   * @link http://www.nlm.nih.gov/bsd/licensee/elements_descriptions.html#pubdate
+   *
+   * @param EntityMetadataWrapper $wrapper
+   *   The wrapped Biblio.
+   * @param $property_name
+   *   The propery name (e.g. biblio_keywords).
+   * @param $data
+   *   A single PubMed article to be processed.
+   * @param $pubmed_data
+   *   A single PubMed article data extracted from "PubmedData".
+   */
+  public function importYear(EntityMetadataWrapper $wrapper, $property_name, SimpleXMLElement $data, SimpleXMLElement $pubmed_data) {
+    $pub_date = $data->Article->Journal->JournalIssue->PubDate;
+
+    if ($pub_date->Year) {
+      $year = (string) $pub_date->Year;
+    }
+    elseif ($pub_date->MedlineDate) {
+      $year = substr((string)$pub_date->MedlineDate, 0, 4);
+    }
+
+    $wrapper->{$property_name}->set($year);
+  }
+
+  /**
+   * Import secondary title.
+   *
+   * @param EntityMetadataWrapper $wrapper
+   *   The wrapped Biblio.
+   * @param $property_name
+   *   The propery name (e.g. biblio_year).
+   * @param $data
+   *   A single PubMed article to be processed.
+   * @param $pubmed_data
+   *   A single PubMed article data extracted from "PubmedData".
+   */
+  public function importSecondaryTitle(EntityMetadataWrapper $wrapper, $property_name, SimpleXMLElement $data, SimpleXMLElement $pubmed_data) {
+    if (!empty($data->MedlineJournalInfo->MedlineTA)) {
+      $title = $data->MedlineJournalInfo->MedlineTA;
+    }
+    elseif (!empty($data->Article->Journal->ISOAbbreviation)) {
+      $title = $data->Article->Journal->ISOAbbreviation;
+    }
+    else {
+      $title = $data->Article->Journal->Title;
+    }
+
+    $wrapper->{$property_name}->set((string)$title);
+  }
+
+  /**
+   * @inheritdoc
+   */
+  public function getMapping() {
+    $return = parent::getMapping();
+
+    $return['field'] = array(
+      /*
+
+      'biblio_citekey'  => $citekey,
+      'biblio_pubmed_id' => $this->id,
+      'biblio_pubmed_md5' => $this->md5,
+      'biblio_contributors' => $this->contributors(),
+
+       */
+
+      'biblio_abstract' => array(
+        'import_method' => 'importAbstract',
+      ),
+      'biblio_alternate_title' => array(
+        'import_location' => array('Journal', 'ISOAbbreviation'),
+      ),
+      'biblio_doi' => array(
+        'import_method' => 'importDoi',
+      ),
+      'biblio_secondary_title' => array(
+        'import_method' => 'importSecondaryTitle',
+      ),
+      'biblio_issn' => array(
+        'import_location' => array('Journal', 'ISSN'),
+      ),
+      'biblio_issue' => array(
+        'import_location' => array('Journal', 'JournalIssue', 'Issue'),
+      ),
+      'biblio_keywords' => array(
+        'import_method' => 'importKeywords',
+      ),
+      'biblio_language' => array(
+        'import_location' => array('Article', 'Language'),
+      ),
+      'biblio_pages' => array(
+        'import_location' => array('Pagination', 'MedlinePgn'),
+      ),
+      'biblio_volume' => array(
+        'import_location' => array('Journal', 'JournalIssue', 'Volume'),
+      ),
+      'biblio_year' => array(
+        'import_method' => 'importYear',
+      ),
+      // @todo: Where should we map this?
+      // 'biblio_custom1' => "http://www.ncbi.nlm.nih.gov/pubmed/{$this->id}?dopt=Abstract",
+      'title' => array(
+        'import_location' => array('Article', 'ArticleTitle'),
+      ),
     );
+
+    // Assign default import method.
+    foreach ($return['field'] as $key => $value) {
+      $return['field'][$key] += array(
+        'import_method' => 'importEntryGeneric',
+        'render_method' => 'renderEntryGeneric',
+      );
+    }
+
+    return $return;
   }
 }
