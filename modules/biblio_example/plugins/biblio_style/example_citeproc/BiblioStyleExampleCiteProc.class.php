498f79ea48c729bb88954183576c5ce6f328e369
Amitaibu <amitai@gizra.com>
2013-12-29 11:19:27 +0200

    Allow setting the style options in \biblio_handler_field_biblio_render::render() from a variable.

--- modules/biblio_example/plugins/biblio_style/example_citeproc/BiblioStyleExampleCiteProc.class.php
+++ modules/biblio_example/plugins/biblio_style/example_citeproc/BiblioStyleExampleCiteProc.class.php
@@ -10,13 +10,6 @@ class BiblioStyleExampleCiteProc extends BiblioStyleCiteProc {
   public function render($options = array(), $langcode = NULL) {
     $output = parent::render($options, $langcode);
 
-    // Get citation.
-    if (empty($this->biblio->title_no_url)) {
-      // Convert the title to a URL referencing the bilbio.
-      $url = entity_uri('biblio', $this->biblio);
-      $citation = str_replace($this->biblio->title, l($this->biblio->title, $url['path'], $url['options']), $output);
-    }
-
     // Get abstract.
     $wrapper = entity_metadata_wrapper('biblio', $this->biblio);
     $abstract = isset($wrapper->biblio_abstract) ? $wrapper->biblio_abstract->value() : '';
@@ -37,7 +30,7 @@ class BiblioStyleExampleCiteProc extends BiblioStyleCiteProc {
     $variables = array(
       'bid' => $wrapper->getIdentifier(),
       'image' => theme('image', array('path' => $image['uri'], 'width' => 50, 'height' => 50)),
-      'citation' => $citation,
+      'title' => $this->biblio->title,
       'abstract' => $abstract,
       'pdf_list' => theme('item_list', array('items' => $items)),
     );
